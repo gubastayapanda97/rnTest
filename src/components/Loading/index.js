7aeac977a79c6fcf169ff48f76cb8ba96e656beb
GubastayaPanda <gubastayapanda97@gmail.com>
2020-07-10 13:23:27 +0300

    add loading indicator component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react';
+import {View, Animated} from 'react-native';
+
+import WaitingIndicator from '../../../assets/images/waitingIndicator.png';
+
+import styles from './style';
+
+const Loading = ({isLoading = true}) => {
+  const spinValue = new Animated.Value(0);
+
+  const animate = () => {
+    Animated.sequence([
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1000,
+      }),
+    ]).start(() => {
+      spinValue.setValue(0);
+      animate();
+    });
+  };
+
+  useEffect(() => {
+    animate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  return isLoading ? (
+    <View style={styles.container}>
+      <Animated.Image
+        style={{...styles.indicatorIcon, transform: [{rotate: spin}]}}
+        source={WaitingIndicator}
+      />
+    </View>
+  ) : null;
+};
+
+export default Loading;
