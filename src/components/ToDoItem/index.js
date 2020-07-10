a34ed9f7a02b9eda6a0174a16c6c2d53b48e05cb
GubastayaPanda <gubastayapanda97@gmail.com>
2020-07-10 13:23:12 +0300

    add todo item component

---
+++ src/components/ToDoItem/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Swipeout from 'react-native-swipeout';
+
+import styles from './style';
+
+const ToDoItem = ({key, item, separators, deteleToDoItem}) => {
+  const swipeoutBtns = [
+    {
+      text: 'Удалить',
+      type: 'delete',
+      onPress: () => deteleToDoItem(item.id),
+    },
+  ];
+
+  return (
+    <View key={key} style={styles.container}>
+      <Swipeout right={swipeoutBtns} left={swipeoutBtns}>
+        <View
+          style={{
+            ...styles.ToDoItemContainer,
+            backgroundColor: item.completed ? '#33ff33' : '#ffffff',
+          }}>
+          <Text>{item.title}</Text>
+        </View>
+      </Swipeout>
+    </View>
+  );
+};
+
+export default ToDoItem;
---
+++ src/components/ToDoItem/style.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 20,
+    marginVertical: 5,
+  },
+  ToDoItemContainer: {
+    borderRadius: 6,
+    borderWidth: 1,
+    borderColor: '#676e69',
+    padding: 10,
+  },
+});
+
+export default styles;
