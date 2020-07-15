0e0e1bbb0bcea67c850bcaa91ca36b39e7763142
GubastayaPanda <gubastayapanda97@gmail.com>
2020-07-16 00:44:02 +0300

    remove disable pretier

--- src/navigation/HeaderLeft.js
+++ src/navigation/HeaderLeft.js
@@ -1,23 +1,22 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
-import { useNavigation, DrawerActions } from '@react-navigation/native';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {useNavigation, DrawerActions} from '@react-navigation/native';
 import Menu from '../../assets/images/menu.png';
 
 import styles from './style';
 
 const HeaderLeft = () => {
-	const navigation = useNavigation();
-	return (
-		<View>
-			<TouchableOpacity
-				onPress={() => {
-					navigation.dispatch(DrawerActions.toggleDrawer());
-				}}>
-				<Image source={Menu} style={styles.drawerIcon} />
-			</TouchableOpacity>
-		</View>
-	);
+  const navigation = useNavigation();
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.dispatch(DrawerActions.toggleDrawer());
+        }}>
+        <Image source={Menu} style={styles.drawerIcon} />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default HeaderLeft;
